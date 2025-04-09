01624e1f82fe62136bedea1d94e6c5591ffd925a
sagar pal <sagarpal252525@gmail.com>
2025-04-09 08:32:06 +0530

    import ki form me dotenv ki file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import connectDB from "./db";
+dotenv.config({
+  path: "./env",
+});
 
 connectDB();
-
-
-
-
-
 
 
 
